3e9343e6bf86fbf233f7b11f1f90673feb1c0cba
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-12-16 22:11:54 +0200

    update FeedbackOptions styles

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -11,7 +11,6 @@ padding: 20px;`
 
 export const Button = styled.button`
 cursor: pointer;
-border: none;
 display: inline-block;
 width: 150px;
 height: 50px;
@@ -19,8 +18,17 @@ padding: 15px;
 margin: 10px 20px;
 border-radius: 15px;
 color: yellow;
+border: 1px solid rgb(230, 228, 223);
 box-shadow: 0 0 40px 40px #F137A6 inset, 0 0 0 0 #F137A6;
 font-family: 'Montserrat', sans-serif;
 font-weight: bold;
 letter-spacing: 2px;
-text-decoration: none;`
+text-decoration: none;
+
+&:hover {
+color: gray;
+box-shadow: rgb(0 0 0 / 10%) 0px 3px 3px -2px,
+        rgb(0 0 0 / 7%) 0px 3px 4px 0px,
+        rgb(0 0 0 / 177%) 0px 1px 8px 0px;
+}
+`
