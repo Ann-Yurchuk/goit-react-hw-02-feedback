faa4db49f5154ee19eb527936e1913c1e6acac62
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-12-16 22:32:06 +0200

    update style

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -11,6 +11,7 @@ padding: 20px;`
 
 export const Button = styled.button`
 cursor: pointer;
+border: none;
 display: inline-block;
 width: 150px;
 height: 50px;
@@ -18,7 +19,7 @@ padding: 15px;
 margin: 10px 20px;
 border-radius: 15px;
 color: yellow;
-border: 1px solid rgb(230, 228, 223);
+// border: 1px solid rgb(230, 228, 223);
 box-shadow: 0 0 40px 40px #F137A6 inset, 0 0 0 0 #F137A6;
 font-family: 'Montserrat', sans-serif;
 font-weight: bold;
@@ -27,8 +28,6 @@ text-decoration: none;
 
 &:hover {
 color: gray;
-box-shadow: rgb(0 0 0 / 10%) 0px 3px 3px -2px,
-        rgb(0 0 0 / 7%) 0px 3px 4px 0px,
-        rgb(0 0 0 / 177%) 0px 1px 8px 0px;
+box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);
 }
 `
